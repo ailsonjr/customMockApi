8b2875080e2b1923b47411a5f5472b9d37a78363
anderleand <andersonleandro.desouza@gmail.com>
2022-11-17 12:52:57 -0300

    ajuste no result

--- result.js
+++ result.js
@@ -1,15 +1,13 @@
 const custom_mock_apis = require('./custom_mock_api');
 const custom_cpfs_mockados = require('./custom_cpfs_mockados');
 
-const responses = custom_mock_apis('13170849697');
+custom_mock_apis().then(result => {
+  custom_cpfs_mockados({ input: '13170849697', responses: result }).then(result => {
+    console.log(JSON.stringify(result, null, 2));
+  }).catch(e => {
+    console.log(e);
+  });
+}).catch(e => {
+  console.log(e);
+})
 
-const params = {
-  input: '13170849697',
-  responses
-}
-
-console.log(params);
-
-custom_cpfs_mockados(params).then(result => {
-  console.log(result);
-});
